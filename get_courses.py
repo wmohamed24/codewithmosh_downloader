2566be90944af25a5838f03c226da2736f9b9c35
Wael Mohamed <wmohamed@colgate.edu>
2023-09-10 02:47:05 -0400

    Final

--- get_courses.py
+++ get_courses.py
@@ -1,6 +1,18 @@
 from getCoursesClass import getCoursesClass
 import os
 from pathlib import Path
+from selenium import webdriver
+import time
+
+
+def get_to_login():
+
+    options = webdriver.ChromeOptions()
+    options.debugger_address = "127.0.0.1:9222"
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://sso.teachable.com/secure/146684/identity/login/password")
+    driver.quit()
+
 
 
 def get_downloads_path():
@@ -14,6 +26,18 @@ def get_downloads_path():
     else:
         raise NotImplementedError(f"Unsupported OS: {platform_system}")
     
+def download_courses(get_courses:getCoursesClass, course_ids, course_titles):
+    
+    for id, title in zip(course_ids, course_titles):
+        get_courses.get_course_entry(id)
+        time.sleep(1)
+        get_courses.get_course_all(title)
+
+
+
+
+    
+    
 
 def main():
     download_path = get_downloads_path()
@@ -21,13 +45,28 @@ def main():
     if not os.path.exists(course_path):
         os.mkdir(course_path)
 
-    get_courses: getCoursesClass = getCoursesClass("127.0.0.1:9222", download_path, course_path)
-    
-    # course_link = "https://members.codewithmosh.com/courses/1120640/lectures/24393589"
-    # check_link = "https://members.codewithmosh.com/courses/1120640/lectures/"
-    # get_courses.get_course_all("Git", course_link, check_link)
+    get_to_login()
+    time.sleep(3)
 
+    get_courses: getCoursesClass = getCoursesClass("127.0.0.1:9222", download_path, course_path)
     get_courses.login()
+
+    course_ids, course_titles = get_courses.get_course_list()
+    if 'Complete_Python_Mastery' in course_titles:
+        course_ids, course_titles = course_ids[1:], course_titles[1:]
+
+    id_title_map = dict(zip(course_ids, course_titles))
+
+    #to download all
+    #download_courses(get_courses, course_ids, course_titles)
+
+    ids_to_download = ['1779784']
+    titles_to_download = [id_title_map[key] for key in ids_to_download if key in id_title_map]
+
+    download_courses(get_courses, ids_to_download, titles_to_download)
+
+    
+
     
 
 
