e805f86f071c01fa4354bc12e3af8bb8972bcee4
Wael Mohamed <wmohamed@colgate.edu>
2023-09-17 00:16:51 -0400

    fix issues

--- get_courses.py
+++ get_courses.py
@@ -26,9 +26,10 @@ def get_downloads_path():
     else:
         raise NotImplementedError(f"Unsupported OS: {platform_system}")
     
-def download_courses(get_courses:getCoursesClass, course_ids, course_titles):
+def download_courses(get_courses:getCoursesClass, id_title_map):
     
-    for id, title in zip(course_ids, course_titles):
+    for id, title in id_title_map.items():
+        print(id, title)
         get_courses.get_course_entry(id)
         time.sleep(1)
         get_courses.get_course_all(title)
@@ -56,17 +57,29 @@ def main():
         course_ids, course_titles = course_ids[1:], course_titles[1:]
 
     id_title_map = dict(zip(course_ids, course_titles))
+    id_title_map.pop("417695", None)
+    id_title_map.pop("525068", None)
+    id_title_map.pop("2037633", None)
+    id_title_map.pop("2178940", None)
+    id_title_map.pop("2037633", None)
+    id_title_map.pop("2074069", None)
+    id_title_map.pop("1779784", None)
+
 
     #to download all
-    #download_courses(get_courses, course_ids, course_titles)
+    download_courses(get_courses, id_title_map)
 
-    ids_to_download = ['1779784']
-    titles_to_download = [id_title_map[key] for key in ids_to_download if key in id_title_map]
 
-    download_courses(get_courses, ids_to_download, titles_to_download)
+    # ids_to_download = ['1779784']
+    # titles_to_download = [id_title_map[key] for key in ids_to_download if key in id_title_map]
+
+    # download_courses(get_courses, ids_to_download, titles_to_download)
+
+    #"C:\Program Files\Google\Chrome\Application\chrome.exe" --remote-debugging-port=9222 --user-data-dir="E:\git\codewithmosh_downloader\ChromeSession"
+
+
 
     
-
     
 
 
